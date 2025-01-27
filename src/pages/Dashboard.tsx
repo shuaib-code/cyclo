6995bef5bb0deaa9f487f5a243c543c6be29a9b5
shuaib-code <shuaib.cyclist@gmail.com>
2025-01-28 01:50:29 +0600

    feat: dynamic Breadcrumb for dashboard 🎉

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -13,7 +13,8 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { Outlet } from "react-router";
+import React from "react";
+import { Link, Outlet, useLocation } from "react-router";
 
 export default function Dashboard() {
   return (
@@ -33,21 +34,45 @@ function DashboardHeader() {
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">
-                Building Your Application
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+        <BreadcrumbGenerator />
       </div>
     </header>
+  );
+}
+
+function BreadcrumbGenerator() {
+  const location = useLocation();
+  const paths = location.pathname.split("/").filter(Boolean);
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/">Home</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+
+        {paths.map((path, index) => {
+          const to = "/" + paths.slice(0, index + 1).join("/");
+
+          return (
+            <React.Fragment key={to}>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                {index === paths.length - 1 ? (
+                  <BreadcrumbPage>{decodeURIComponent(path)}</BreadcrumbPage>
+                ) : (
+                  <BreadcrumbLink asChild>
+                    <Link to={to}>{decodeURIComponent(path)}</Link>
+                  </BreadcrumbLink>
+                )}
+              </BreadcrumbItem>
+            </React.Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
   );
 }
 
